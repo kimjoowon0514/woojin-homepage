b6ea0ac01e25e6f3956f732f5228646bb626b743
kimjoowon0514 <alphalotus@naver.com>
2025-07-03 10:29:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
+
+document.querySelector("form").addEventListener("submit", function(e) {
   e.preventDefault();
-  document.getElementById('formStatus').textContent = "문의가 성공적으로 접수되었습니다. 감사합니다!";
-  this.reset();
+  alert("문의가 정상적으로 접수되었습니다. 감사합니다!");
 });
